6f8addf376e3be2bf1307589d71238051f48a204
slowbut-sure <thanhtruongdinh2002@gmail.com>
2024-05-29 21:45:51 +0700

    add file service

--- CapstoneProject_FPT_Summer2024/StudentSupervisorService/DependencyInjection.cs
+++ CapstoneProject_FPT_Summer2024/StudentSupervisorService/DependencyInjection.cs
@@ -14,6 +14,7 @@ using Microsoft.EntityFrameworkCore;
 using StudentSupervisorService.Service;
 using StudentSupervisorService.Service.Implement;
 using StudentSupervisorService.Mapper;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace StudentSupervisorService
 {
@@ -30,11 +31,83 @@ namespace StudentSupervisorService
             //Add DI Container
             services.AddTransient<IUnitOfWork, UnitOfWork>();
 
+            services.AddTransient<IClassGroupRepository, ClassGroupRepository>();
+            services.AddTransient<ClassGroupService, ClassGroupImplement>();
+
+            services.AddTransient<IClassReposirory, ClassRepository>();
+            services.AddTransient<ClassService, ClassImplement>();
+
+            services.AddTransient<IDisciplineRepository, DisciplineRepository>();
+            services.AddTransient<DisciplineService, DisciplineImplement>();
+
+            services.AddTransient<IEvaluationRepository, EvaluationRepository>();
+            services.AddTransient<EvaluationService, EvaluationImplement>();
+
+            services.AddTransient<IEvaluationDetailRepository, EvaluationDetailRepository>();
+            services.AddTransient<EvaluationDetailService, EvaluationDetailImplement>();
+
             services.AddTransient<IHighSchoolRepository, HighSchoolRepository>();
             services.AddTransient<HighSchoolService, HighSchoolImplement>();
 
+            services.AddTransient<IImageUrlRepository, ImageUrlRepository>();
+            services.AddTransient<ImageUrlService, ImageUrlImplement>();
+
+            services.AddTransient<IPackageRepository, PackageRepository>();
+            services.AddTransient<PackageService, PackageImplement>();
+
+            services.AddTransient<IPatrolScheduleRepository, PatrolScheduleRepository>();
+            services.AddTransient<PatrolScheduleService, PatrolScheduleImplement>();
+
+            services.AddTransient<IPenaltyRepository, PenaltyRepository>();
+            services.AddTransient<PenaltyService, PenaltyImplement>();
+
+            services.AddTransient<IRegisteredSchoolRepository, RegisteredSchoolRepository>();
+            services.AddTransient<RegisteredSchoolService, RegisteredSchoolImplement>();
+
+            services.AddTransient<ISchoolAdminRepository, SchoolAdminRepository>();
+            services.AddTransient<SchoolAdminService, SchoolAdminImplement>();
+
+            services.AddTransient<ISchoolConfigRepository, SchoolConfigRepository>();
+            services.AddTransient<SchoolConfigService, SchoolConfigImplement>();
+
             services.AddTransient<ISchoolYearRepository, SchoolYearRepository>();
             services.AddTransient<SchoolYearService, SchoolYearImplement>();
+
+            services.AddTransient<IStudentRepository, StudentRepository>();
+            services.AddTransient<StudentService, StudentImplement>();
+
+            services.AddTransient<IStudentInClassRepository, StudentInClassRepository>();
+            services.AddTransient<StudentInClassService, StudentInClassImplement>();
+
+            services.AddTransient<IStudentSupervisorRepository, StudentSupervisorRepository>();
+            services.AddTransient<StudentSupervisorServices, StudentSupervisorImplement>();
+
+            services.AddTransient<ITeacherRepository, TeacherRepository>();
+            services.AddTransient<TeacherService, TeacherImplement>();
+
+            services.AddTransient<ITimeRepository, TimeRepository>();
+            services.AddTransient<TimeService, TimeImplement>();
+
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<UserService, UserImplement>();
+
+            services.AddTransient<IViolationRepository, ViolationRepository>();
+            services.AddTransient<ViolationService, ViolationImplement>();
+
+            services.AddTransient<IViolationConfigRepository, ViolationConfigRepository>();
+            services.AddTransient<ViolationConfigService, ViolationConfigImplement>();
+
+            services.AddTransient<IViolationGroupRepository, ViolationGroupRepository>();
+            services.AddTransient<ViolationGroupService, ViolationGroupImplement>();
+
+            services.AddTransient<IViolationReportRepository, ViolationReportRepository>();
+            services.AddTransient<ViolationReportService, ViolationReportImplement>();
+
+            services.AddTransient<IViolationTypeRepository, ViolationTypeRepository>();
+            services.AddTransient<ViolationTypeService, ViolationTypeImplement>();
+
+            services.AddTransient<IYearPackageRepository, YearPackageRepository>();
+            services.AddTransient<YearPackageService, YearPackageImplement>();
 
 
             services.AddTransient<IServiceCollection, ServiceCollection>();
