b9887d12b186343ba18670587976f3ed7bba391d
Thành Trương <122356463+slowbut-sure@users.noreply.github.com>
2024-06-07 14:09:07 +0700

    hide swagger

--- CapstoneProject_FPT_Summer2024/StudentSupervisorAPI/Program.cs
+++ CapstoneProject_FPT_Summer2024/StudentSupervisorAPI/Program.cs
@@ -79,11 +79,11 @@ builder.Services.AddAuthentication(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
