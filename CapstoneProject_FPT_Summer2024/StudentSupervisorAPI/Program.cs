d0e7bfce4a19b4c1b8e2497f2f96c8dcb79fab1e
tptanphuoc <huynhphuoc2468@gmail.com>
2024-07-19 14:52:40 +0700

    demo checkout function

--- CapstoneProject_FPT_Summer2024/StudentSupervisorAPI/Program.cs
+++ CapstoneProject_FPT_Summer2024/StudentSupervisorAPI/Program.cs
@@ -4,15 +4,32 @@ using dotenv.net;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Net.payOS;
 using Newtonsoft.Json;
 using StudentSupervisorAPI.Cofiguration;
 using StudentSupervisorService;
 using System.Text;
 
+IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+PayOS payOS = new PayOS(configuration["Environment:PAYOS_CLIENT_ID"] ?? throw new Exception("Cannot find environment"),
+                    configuration["Environment:PAYOS_API_KEY"] ?? throw new Exception("Cannot find environment"),
+                    configuration["Environment:PAYOS_CHECKSUM_KEY"] ?? throw new Exception("Cannot find environment"));
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add DI Services
 builder.Services.AddDIServices(builder.Configuration);
+builder.Services.AddSingleton(payOS);
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
+        });
+});
+
 
 builder.Services.AddControllers();
 ServiceConfigurations.ConfigureSwagger(builder.Services);
@@ -32,6 +49,7 @@ app.UseCors(x => x
           .AllowCredentials());
 
 app.UseHttpsRedirection();
+app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
 
---
+++ CapstoneProject_FPT_Summer2024/StudentSupervisorService/PayOSConfig/PayOSConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSupervisorService.PayOSConfig
+{
+    public class PayOSConfig
+    {
+        public static readonly string RETURN_URL = "https://school-fe-admin-main.vercel.app/";
+        public static readonly string CANCEL_URL = "https://school-fe-admin-main.vercel.app/";
+    }
+}
